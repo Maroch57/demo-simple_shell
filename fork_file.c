2c89cfc02e3927e3230079c2fcdcaa861403f34a
Maroch57 <jahathblessed@gmail.com>
2023-12-06 02:25:31 +0300

    wait system call

--- fork_file.c
+++ fork_file.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
 int main(void)
 {
 	pid_t pid;
-	pid_t ppid;
+	//pid_t ppid;
 
 	/*written before fork was called */
-
-	/*called fork */
 	//printf("Before fork I was one\n");
-
+	
+	/* child process is created */
 	pid = fork();
 
+	/* if  fork fails */
 	if (pid == -1)
 	{
 		perror("Unsuccessful\n");
@@ -21,14 +24,20 @@ int main(void)
 
 	//printf("After fork I became two\n");
 
+	/* in child process */
 	if (pid == 0)
 	{
-		sleep(40);
-		printf("I am the child\n");
+		//sleep(40);
+		printf("I am the child process\n");
 	}
+	/* in parent process */
 	else
-		ppid = getppid();
-		printf("Parent pid is: %u\n", ppid);
+	{
+		//ppid = getppid();
+		wait(NULL);
+		sleep(30);
+		printf("Parent process\n");
+	}
 
 	return (0);
 }
